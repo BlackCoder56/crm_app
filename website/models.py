f69756d01ec5d7de5c47d4f21be7a8a74a485c76
Elly <blackcoda56@gmail.com>
2023-09-03 08:09:54 +0300

    Displaying models on page with bootstrap version

--- website/models.py
+++ website/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Record(models.Model):
+
     created_at = models.DateTimeField(auto_now=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -14,4 +15,4 @@ class Record(models.Model):
     zipcode = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name}  {self.last_name}"
