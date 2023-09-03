dde7ddc560712f151090a45d132b036a881c3c08
Elly <blackcoda56@gmail.com>
2023-09-03 04:35:15 +0300

    Register user version

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from . forms import SignUpForm
 
 
 # Create your views here.
@@ -25,4 +26,24 @@ def home(request):
 
 def logout_user(request):
     logout(request)
+    messages.success(request, "You have logged out successfully!")
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authentication and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome.")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
